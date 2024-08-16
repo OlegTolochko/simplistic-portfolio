e7b167dd76685a986b1408356b26e3e3f788c8f1
Oleg Tolochko <oleg.i.tolochko@gmail.com>
2024-08-16 13:19:12 +0200

    create basic layout for portfolio

---
+++ simplistic-portfolio/app/components/header.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="header__content">
+                <h1 className="header__title">Simplistic Portfolio</h1>
+                <p className="header__subtitle">A simple portfolio template</p>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
