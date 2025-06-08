56e5e293e6f28fedb1b23f1c22e2a5112e075a2f
Oleg Tolochko <oleg.i.tolochko@gmail.com>
2025-06-08 16:58:10 +0200

    add hover animation to projects

--- app/components/project.tsx
+++ app/components/project.tsx
@@ -129,6 +129,16 @@ const Project: React.FC<ProjectProps> = ({ index, name, description, skills, img
     className="rounded-3xl flex flex-col border-2 p-4 border-sand-500 bg-sand-200 col-span-1 overflow-hidden md:w-[50%]"
     onHoverStart={() => !isMobile && handleHoverStart()}
     onHoverEnd={() => !isMobile && handleHoverEnd()}
+    whileHover={{
+      y: -8,
+      boxShadow: "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)",
+      transition: {
+        type: "spring",
+        mass: 1,
+        stiffness: 200,
+        damping: 20,
+      },
+    }}
     onClick={handleClick}
 >
       <div className="flex-1">
